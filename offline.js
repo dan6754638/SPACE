ba6cc0fee058ef81d38c5b921c9bcfafeef43ddb
dan6754638 <150989841+dan6754638@users.noreply.github.com>
2023-11-23 09:20:20 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1700713123,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/Спрайт-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/ракета-sheet0.png",
+		"images/ракета-sheet1.png",
+		"images/Спрайт4-sheet0.png",
+		"images/Спрайт6-sheet0.png",
+		"images/Спрайт7-sheet0.png",
+		"images/Частицы.png",
+		"images/Спрайт3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
